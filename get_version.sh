330d5cc68eddbc4a27bf07d1472a86dea8631ce3
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-26 18:30:28 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# File where the current version is stored
-VERSION_FILE="version.txt"
-
-# Read the current version from the file
-if [ ! -f $VERSION_FILE ]; then
-    echo "0.0.0" > $VERSION_FILE
+# Check if VERSION file exists
+if [ -f VERSION ]; then
+    # Read the current version from the file
+    CURRENT_VERSION=$(cat VERSION)
+    # Split the version into its components (assuming vMAJOR.MINOR.PATCH format)
+    MAJOR=$(echo $CURRENT_VERSION | cut -d '.' -f 1)
+    MINOR=$(echo $CURRENT_VERSION | cut -d '.' -f 2)
+    PATCH=$(echo $CURRENT_VERSION | cut -d '.' -f 3)
+    
+    # Increment the patch version
+    PATCH=$((PATCH + 1))
+    
+    # Form the new version string
+    NEW_VERSION="v$MAJOR.$MINOR.$PATCH"
+else
+    # If VERSION file does not exist, start with v0.0.1
+    NEW_VERSION="v0.0.1"
 fi
-
-CURRENT_VERSION=$(cat $VERSION_FILE)
-
-# Increment the patch version
-IFS='.' read -r -a VERSION_PARTS <<< "$CURRENT_VERSION"
-VERSION_PARTS[2]=$((VERSION_PARTS[2] + 1))
-
-# New version
-NEW_VERSION="${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.${VERSION_PARTS[2]}"
-
-# Write the new version to the file
-echo $NEW_VERSION > $VERSION_FILE
 
 # Output the new version
 echo $NEW_VERSION
+
+# Write the new version back to VERSION file for next build
+echo $NEW_VERSION > VERSION
