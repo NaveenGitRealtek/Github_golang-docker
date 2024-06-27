55977d07c71c4f0c15016e50c23c70e51b97da34
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-27 11:31:00 +0530

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -5,7 +5,7 @@ if [ -f VERSION ]; then
     # Read the current version from the file
     CURRENT_VERSION=$(cat VERSION)
     # Split the version into its components (assuming vMAJOR.MINOR.PATCH format)
-    MAJOR=$(echo $CURRENT_VERSION | cut -d '.' -f 1 | cut -d 'v' -f 2)
+    MAJOR=$(echo $CURRENT_VERSION | cut -d '.' -f 1)
     MINOR=$(echo $CURRENT_VERSION | cut -d '.' -f 2)
     PATCH=$(echo $CURRENT_VERSION | cut -d '.' -f 3)
     
@@ -21,11 +21,3 @@ fi
 
 # Output the new version
 echo $NEW_VERSION
-
-# Update the VERSION file
-echo $NEW_VERSION > VERSION
-
-# Build and push the Docker image with the new version
-docker build -t naveen775/my-go-app1:$NEW_VERSION .
-docker push naveen775/my-go-app1:$NEW_VERSION
-
