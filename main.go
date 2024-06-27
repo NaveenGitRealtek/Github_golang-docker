b1117119200fa1ebf7b74936aed587357169d82c
NaveenGitRealtek <153925033+NaveenGitRealtek@users.noreply.github.com>
2024-06-27 14:06:13 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, World!!!!")
     fmt.Fprintf(w,  " Hey Docker-Hub Iam Here..")
+    fmt.Fprintf(w,  " Bye")
 }
 
 func main() {
